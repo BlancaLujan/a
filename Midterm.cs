a97ffa75daf8656d2ba56bf6abe894989bec9c2f
BlancaLujan <77300324+BlancaLujan@users.noreply.github.com>
2021-01-25 19:07:31 -0700

    Add files via upload

---
+++ Midterm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Program.File.MyClasses
+{
+    public class Midterm
+    {
+        public Midterm()
+        {
+            Console.WriteLine("1. Create a User");
+            Console.WriteLine("2. Create a Comment");
+            Console.WriteLine("3. Print a List of Users");
+            Console.WriteLine("4. print a List of Comments");
+            Console.WriteLine("5. Exit the program");
+
+            Console.WriteLine("");
+            Console.WriteLine("Please select a value");
+
+            string response = Console.ReadLine();
+
+            Console.WriteLine("your response is: " + response);
+        }
+    }
+}
